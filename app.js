f84ca228ec2bf1006d046f9a2432158cb275e724
DevSiddhartha <sidkittu10@gmail.com>
2021-01-31 23:58:55 +0530

    taking input and console

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 // alert("pyar h aap hmara");
 // var username = prompt("Give me your username");
 // alert("hello " + username);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+// console.log(txtInput)
+
+function clickHandler(){
+    console.log("clicked");
+    console.log(txtInput.value);
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
