e4680416cab5ebd86fbf79106d298b728e79f4c7
DevSiddhartha <sidkittu10@gmail.com>
2021-02-01 01:06:06 +0530

    input output procesing done

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// console.log("Hello! script is working from a different source")
-// alert("pyar h aap hmara");
-// var username = prompt("Give me your username");
-// alert("hello " + username);
 
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 // console.log(txtInput)
 
 function clickHandler(){
-    console.log("clicked");
-    console.log(txtInput.value);
-
+    outputDiv.innerText = "asasasas " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler)
